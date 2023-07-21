b982c3f7c303bad701c79956bc5149ef472b9c60
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2023-07-21 14:50:48 +0100

    upload image user

--- Routes/profileEdit.js
+++ Routes/profileEdit.js
@@ -1,11 +1,21 @@
 import express from 'express';
 const router =express.Router()
 
+import multer from 'multer';  
 
 import * as authController from '../Controllers/AuthController.js';
 import * as profileEdit from '../Controllers/ProfileController.js';
 
+import { storage } from '../Controllers/ProfileController.js';
+const Upload=multer({storage:storage});
+
+
+
 router.post('/edit/:id',profileEdit.updateUser)
+router.post('/editImage/:id',Upload.single('image'),profileEdit.updateUser)
 router.post('/editPassword/:id',profileEdit.updatePassword)
+
+
+
 
   export default router;
