4a108d9047b92caeb9e6c04c846c2aa40534e884
shayma5 <shayma.ettaieb@esprit.tn>
2023-07-21 15:15:59 +0100

    reset password

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 import * as authController from '../Controllers/AuthController.js';
 import * as profileEdit from '../Controllers/ProfileController.js';
-import * as forgotpassword from '../Controllers/ForgotPassword.js'
+import * as forgotpassword from '../Controllers/ForgotPassword.js';
 
 router.post('/registerClient', authController.registerC);
 router.post('/registerFourniseur', authController.registerF);
 router.post('/login', authController.login);
 router.post('/forgotpassword',forgotpassword.forget_password);
+router.get('/reset-password',forgotpassword.reset_password);
+
 
 export default router;
