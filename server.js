a12120e52c33ba5ea4d219060ac45d0190618a1b
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2023-07-21 17:23:07 +0100

    deleting files

--- server.js
+++ server.js
@@ -18,6 +18,4 @@ app.use('/api/', authRoute);
 
 
 const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
