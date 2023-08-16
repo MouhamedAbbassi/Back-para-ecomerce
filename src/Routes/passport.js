b2ebaa63048a6a79af08ecd4c77275ef29219373
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2023-08-16 17:13:24 +0100

    some changes

--- src/Routes/passport.js
+++ src/Routes/passport.js
@@ -1,22 +1,26 @@
+
+
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
-  
-passport.serializeUser((user , done) => {
-    done(null , user);
-})
-passport.deserializeUser(function(user, done) {
+
+const setUpPassport = () => {
+  passport.serializeUser((user, done) => {
     done(null, user);
-});
-  
-passport.use(new GoogleStrategy({
+  });
+
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.use(new GoogleStrategy({
     clientID:"682851405471-m6md7aqpdd0v5acq8mmo3nkug08ele7c.apps.googleusercontent.com", // Your Credentials here.
     clientSecret:"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", // Your Credentials here.
-    callbackURL:"https://localhost:3001/auth/google/callback",
-    passReqToCallback:true
+    callbackURL: "http://localhost:3001/auth/google/callback",
+    passReqToCallback: true
   },
-  function(request, accessToken, refreshToken, profile, done) {
+  (request, accessToken, refreshToken, profile, done) => {
     return done(null, profile);
-  }
-));
+  }));
+};
 
-export default passport;
+export default setUpPassport;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-import("./config/connexions.js");  
-//import { swaggerUi, swaggerSpec } from "./config/swagger.js";
+import("./config/connexions.js");
 import morgan from "morgan";
 import path from "path";
 import express from "express";
-import cookieParser from "cookie-parser";  
+import cookieParser from "cookie-parser";
 import { swaggerUi, specs } from "./config/swagger.js";
 import authRoute from "./Routes/Auth.js";
 import ProfileRoutes from "./Routes/ProfileRoutes.js";
@@ -13,85 +12,75 @@ import productRoutes from "./Routes/ProductRoutes.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import crypto from "crypto";
- import passport from "passport";
- import cookieSession from "cookie-session";
+import passport from "passport";
+import cookieSession from "cookie-session";
 import setUpPassport from "./Routes/passport.js";
-import session from "express-session";
-import GoogleStrategy from "passport-google-oauth20";
 
-
- 
-// Get the current directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
- const app = express();
+const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-
 const key1 = crypto.randomBytes(32).toString("hex");
 const key2 = crypto.randomBytes(32).toString("hex");
-app.use(cookieSession({
-  name: "google-auth-session",
-  keys: [key1, key2]
-}));
+app.use(
+  cookieSession({
+    name: "google-auth-session",
+    keys: [key1, key2],
+  })
+);
+
+// Configure Passport using setUpPassport function
+setUpPassport(passport);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
- 
-
-// Enable CORS for specific origins
 const corsOptions = {
-  origin: ["http://127.0.0.1:3001","http://localhost:5173"],
+  origin: ["http://127.0.0.1:3001", "http://localhost:5173"],
 };
-
 app.use(cors(corsOptions));
-
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Serve Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
- 
 
 app.use("/api/", authRoute);
 app.use("/api/", ProfileRoutes);
 app.use("/api/", ParaRoutes);
 app.use("/api/", productRoutes);
 
-////////////////////ABOUT OAUTH/////////////////////
-
 app.get("/", (req, res) => {
   res.send("<button><a href='/auth'>Login With Google</a></button>");
 });
 
-// Auth 
-app.get("/auth" , passport.authenticate("google", { scope:
-  [ "https://www.googleapis.com/auth/userinfo.email", "profile" ]
+app.get("/auth", passport.authenticate("google", {
+  scope: ["https://www.googleapis.com/auth/userinfo.email", "profile"]
 }));
 
-// Auth Callback
-app.get( "/auth/callback",
-  passport.authenticate( "google", {
-      successRedirect: "/auth/callback/success",
-      failureRedirect: "/auth/callback/failure"
+app.get("/auth/callback", passport.authenticate("google", {
+  successRedirect: "/auth/callback/success",
+  failureRedirect: "/auth/callback/failure",
 }));
 
-// Success 
-app.get("/auth/callback/success" , (req , res) => {
-  if(!req.user)
-      res.redirect("/auth/callback/failure");
+app.get("/auth/callback/success", (req, res) => {
+  if (!req.user) res.redirect("/auth/callback/failure");
   res.send("Welcome " + req.user.email);
 });
 
-// failure
-app.get("/auth/callback/failure" , (req , res) => {
+app.get("/auth/callback/failure", (req, res) => {
   res.send("Error");
 });
- 
+
 const PORT = 3001;
-app.listen(PORT, () => { console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
  
- });
+
+
+
+
